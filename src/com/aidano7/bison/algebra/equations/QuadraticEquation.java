1dea9afe321156a8a57b6d7d245bf27e7a8fec4b
Aidan <52678943+aidano-13@users.noreply.github.com>
2021-03-30 12:53:04 -0500

    🟦 Added the QuadraticEquation class.

---
+++ src/com/aidano7/bison/algebra/equations/QuadraticEquation.java
@@ -0,0 +1,25 @@
+package com.aidano7.bison.algebra.equations;
+
+/**
+ * @author aidano-7
+ * @date 3/30/2021
+ *
+ * <p>
+ *     Implementation of a quadratic in Bison. Standard form is
+ *     ax^2 + bx + c = 0.
+ * </p>
+ */
+
+public class QuadraticEquation implements Equation {
+
+    /**
+     * Gets the y value of a function.
+     *
+     * @param x the x value to get
+     * @return y the y value of the function
+     */
+    @Override
+    public float get(float x) {
+        return 0f;
+    }
+}
